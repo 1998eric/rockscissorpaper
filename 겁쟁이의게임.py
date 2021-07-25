a005d1c731de2999678d19ab3d3131ae5f897ca1
1998eric <59193273+1998eric@users.noreply.github.com>
2021-07-25 20:42:52 +0900

    Update 겁쟁이의게임.py

--- 겁쟁이의게임.py
+++ 겁쟁이의게임.py
@@ -2,25 +2,37 @@ import random
 
 
 def rsp(a,b):
+    global tie_count, lose_count, win_count
     if(a==b):
         print("비겼습니다")
+        tie_count += 1
     elif(a=="가위"):
         if(b=="바위"):
             print("님 짐")
+            lose_count += 1
         if(b=="보"):
             print("이겼읍니다.")
+            win_count += 1
     elif(a=="바위"):
         if(b=="보"):
             print("님 짐")
+            lose_count += 1
         if(b=="가위"):
-            print("이겼읍니다.")        
+            print("이겼읍니다.")
+            win_count += 1
+
     elif(a=="보"):
         if(b=="가위"):
             print("님 짐")
+            lose_count += 1
         if(b=="바위"):
             print("이겼읍니다.")
+            win_count += 1
 
-            
+
+win_count = 0
+lose_count = 0            
+tie_count = 0
 
 while True:
     a = input("가위, 바위, 보중에 선택하시오: ")
@@ -33,3 +45,4 @@ while True:
 
     rsp(a,b)
     print("당신은:", a," 상대는: ", b)
+    print("승리:",win_count," 패배:", lose_count, " 동점:", tie_count)
